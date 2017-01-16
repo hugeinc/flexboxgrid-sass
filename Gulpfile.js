0181ccf2289407ec03f6cddbc05fa9fa9ab4723f
JP Blanchette <JBlanchette@globeandmail.com>
2017-01-16 16:00:55 -0500

    seperate flexboxgrid into multiple files

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,11 +7,11 @@ var sass = require('gulp-sass');
 var del = require('del');
 
 var supported_browsers = browserslist('last 4 versions, > 1%, not ie <= 9');
+var supported_browsers_logged = false;
 
 function logBrowserSupport(browsers) {
   console.info('\nVendor prefixes will be applied for the following browsers:\n\n', browsers.join('\n'));
 }
-var supportedBrowsersWereLogged = false;
 
 gulp.task('styles:demo', function () {
   return gulp.src('demo/sass/*')
@@ -27,8 +27,9 @@ gulp.task('styles:demo', function () {
 });
 
 gulp.task('styles:dist', function () {
-  if (!supportedBrowsersWereLogged) {
-    logBrowserSupport(supported_browsers);
+  if (!supported_browsers_logged) {
+    supported_browsers_logged = true;
+    // logBrowserSupport(supported_browsers);
   }
 
   return gulp.src('src/flexboxgrid.scss')
@@ -51,7 +52,7 @@ gulp.task("clean", () => {
 });
 
 gulp.task('watch', ['styles:demo', 'styles:dist'], function () {
-  gulp.watch('*.scss', ['styles:dist']);
+  gulp.watch('src/*.scss', ['styles:dist']);
   gulp.watch('demo/sass/*.scss', ['styles:demo']);
 });
 
